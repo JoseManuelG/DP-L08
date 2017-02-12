ce396f56fd88144bb537b13282d2d05beb37df0a
Javi <trarsheise@gmail.com>
2017-02-13 00:48:15 +0100

    Archivos modificados del Item 2, olvidados de añadir en el commit anterior

--- Item 2/Sample-Project-1.9/src/main/java/utilities/HashPassword.java
+++ Item 2/Sample-Project-1.9/src/main/java/utilities/HashPassword.java
@@ -1,11 +1,11 @@
-/* HashPassword.java
- *
+/*
+ * HashPassword.java
+ * 
  * Copyright (C) 2017 Universidad de Sevilla
  * 
- * The use of this project is hereby constrained to the conditions of the 
- * TDG Licence, a copy of which you may download from 
+ * The use of this project is hereby constrained to the conditions of the
+ * TDG Licence, a copy of which you may download from
  * http://www.tdg-seville.info/License.html
- * 
  */
 
 package utilities;
@@ -24,18 +24,18 @@ public class HashPassword {
 		String line, hash;
 
 		try {
-			System.out.printf("HashPassword 1.8%n");
+			System.out.printf("HashPassword 1.9%n");
 			System.out.printf("----------------%n%n");
-			
+
 			encoder = new Md5PasswordEncoder();
-			reader = new ConsoleReader(); 
-				
+			reader = new ConsoleReader();
+
 			line = reader.readLine();
-			while (!line.equals("quit")) {				
+			while (!line.equals("quit")) {
 				hash = encoder.encodePassword(line, null);
 				System.out.println(hash);
 				line = reader.readLine();
-			} 
+			}
 		} catch (Throwable oops) {
 			System.out.flush();
 			System.err.printf("%n%s%n", oops.getLocalizedMessage());
--- Item 2/Sample-Project-1.9/src/main/java/utilities/QueryDatabase.java
+++ Item 2/Sample-Project-1.9/src/main/java/utilities/QueryDatabase.java
@@ -1,11 +1,11 @@
-/* QueryDatabase.java
- *
+/*
+ * QueryDatabase.java
+ * 
  * Copyright (C) 2017 Universidad de Sevilla
  * 
- * The use of this project is hereby constrained to the conditions of the 
- * TDG Licence, a copy of which you may download from 
+ * The use of this project is hereby constrained to the conditions of the
+ * TDG Licence, a copy of which you may download from
  * http://www.tdg-seville.info/License.html
- * 
  */
 
 package utilities;
@@ -19,24 +19,24 @@ import utilities.internal.CustomToStringBuilder;
 import utilities.internal.DatabaseUtil;
 
 public class QueryDatabase {
-	
+
 	public static void main(String[] args) throws Throwable {
 		DatabaseUtil databaseUtil;
 		ConsoleReader reader;
 		String line;
 		boolean quit;
-		
+
 		databaseUtil = null;
-		
+
 		try {
-			System.out.printf("QueryDatabase 1.8%n");
+			System.out.printf("QueryDatabase 1.9%n");
 			System.out.printf("-----------------%n%n");
-			
+
 			System.out.printf("Initialising persistence context `%s'...%n", DatabaseConfig.PersistenceUnit);
 			databaseUtil = new DatabaseUtil();
-			
+
 			reader = new ConsoleReader();
-			
+
 			do {
 				line = reader.readCommand();
 				quit = interpretLine(databaseUtil, line);
@@ -45,7 +45,7 @@ public class QueryDatabase {
 			System.out.flush();
 			System.err.printf("%n%s%n", oops.getLocalizedMessage());
 			//oops.printStackTrace(System.out);			
-		} finally {					
+		} finally {
 			databaseUtil.close();
 		}
 	}
@@ -55,55 +55,55 @@ public class QueryDatabase {
 		String command;
 		List<?> objects;
 		int affected;
-		
-		result = false;		
-		try {				
+
+		result = false;
+		try {
 			command = StringUtils.substringBefore(line, " ");
-			switch (command) {				
-				case "quit":
-					result = true;						
-					break;
-				case "begin": 
-					databaseUtil.openTransaction();
-					System.out.println("Transaction started");
-					break;
-				case "commit":
-					databaseUtil.commitTransaction();
-					System.out.println("Transaction committed");
-					break;
-				case "rollback": 
-					databaseUtil.rollbackTransaction();
-					System.out.println("Transaction rollbacked");
-					break;
-				case "update":
-				case "delete": 
-					affected = databaseUtil.executeUpdate(line);
-					System.out.printf("%d objects affected%n", affected);
-					break;
-				case "select": 
-					objects = databaseUtil.executeSelect(line);			
-					System.out.printf("%d objects found%n", objects.size());	
-					printResultList(objects);
-					break;
-				default:
-					System.err.println("Command not understood");
+			switch (command) {
+			case "quit":
+				result = true;
+				break;
+			case "begin":
+				databaseUtil.openTransaction();
+				System.out.println("Transaction started");
+				break;
+			case "commit":
+				databaseUtil.commitTransaction();
+				System.out.println("Transaction committed");
+				break;
+			case "rollback":
+				databaseUtil.rollbackTransaction();
+				System.out.println("Transaction rollbacked");
+				break;
+			case "update":
+			case "delete":
+				affected = databaseUtil.executeUpdate(line);
+				System.out.printf("%d objects affected%n", affected);
+				break;
+			case "select":
+				objects = databaseUtil.executeSelect(line);
+				System.out.printf("%d objects found%n", objects.size());
+				printResultList(objects);
+				break;
+			default:
+				System.err.println("Command not understood");
 			}
-		} catch (Throwable oops) {	
+		} catch (Throwable oops) {
 			System.err.println(oops.getMessage());
 			// oops.printStackTrace(System.err);
 		}
-		
+
 		return result;
 	}
 
 	private static void printResultList(List<?> objects) {
-		String text;		
+		String text;
 		Object obj;
-		
+
 		for (int i = 0; i < objects.size(); i++) {
 			obj = objects.get(i);
 			text = CustomToStringBuilder.toString(obj);
-			System.out.printf("Object #%d = %s %n", i, text);			
+			System.out.printf("Object #%d = %s %n", i, text);
 		}
 	}
 
