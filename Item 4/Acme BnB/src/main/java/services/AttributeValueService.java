6b09ed7a4c5343596cd8f88224b37a4384f999c3
Daniel Iglesias Pérez <daniglper@gmail.com>
2017-02-14 22:10:10 +0100

    La authority ADMIN ha pasado a ADMINISTRATOR

--- Item 4/Acme BnB/src/main/java/services/AttributeValueService.java
+++ Item 4/Acme BnB/src/main/java/services/AttributeValueService.java
@@ -1,3 +1,4 @@
+
 package services;
 
 import java.util.Collection;
@@ -12,66 +13,69 @@ import security.UserAccount;
 import domain.AttributeValue;
 
 public class AttributeValueService {
+
 	//Managed Repository---------------------------—
 
-	  @Autowired
-	  private AttributeValueRepository attributeValueRepository;
-	  
-	  //Supporting services---------------------------—
-	  @Autowired
-	  private LoginService loginService;
-	  //Constructors------------------------------------
-	  
-	    public AttributeValueService(){
-	      super();
-	    }
-	    
-	    //Simple CRUD methods----------------------------
-	    
-	    public AttributeValue create() {
-	      AttributeValue result;
-	      
-	      result = new AttributeValue();
-	    
-	      return result;
-	    }
+	@Autowired
+	private AttributeValueRepository	attributeValueRepository;
 
-	    public Collection<AttributeValue> findAll() {
-	      Collection<AttributeValue> result;
+	//Supporting services---------------------------—
+	@Autowired
+	private LoginService				loginService;
 
-	      result = attributeValueRepository.findAll();
-	      Assert.notNull(result);
 
-	      return result;
-	    }
+	//Constructors------------------------------------
 
-	    public AttributeValue findOne(int attributeValueId) {
-	      AttributeValue result;
+	public AttributeValueService() {
+		super();
+	}
 
-	      result = attributeValueRepository.findOne(attributeValueId);
-	      Assert.notNull(result);
+	//Simple CRUD methods----------------------------
 
-	      return result;
-	    }
+	public AttributeValue create() {
+		AttributeValue result;
 
-	    public AttributeValue save(AttributeValue attributeValue) {
-	      Assert.hasText(attributeValue.getValue(), "El atributeValue debe tener un valor");
-	      UserAccount account=loginService.getPrincipal();
-	      Assert.isTrue(account.getAuthorities().contains(Authority.ADMIN), "Debes ser un Administrador para editar los AttributeValues");
-	     
-	      AttributeValue result;
-	      result = attributeValueRepository.save(attributeValue);
-	      
-	      return result;
-	    }
+		result = new AttributeValue();
 
-	    public void delete(AttributeValue attributeValue) {
-	      Assert.notNull(attributeValue,"El attributeValueo no puede ser nulo");
-	      Assert.isTrue(attributeValue.getId() != 0,"El attributeValueo debe estar en la base de datos");
-	      UserAccount account=loginService.getPrincipal();
-	      Assert.isTrue(account.getAuthorities().contains(Authority.ADMIN), "Debes ser un Administrador para editar los AttributeValues");
+		return result;
+	}
 
-	      attributeValueRepository.delete(attributeValue);
-	    }
-	    
+	public Collection<AttributeValue> findAll() {
+		Collection<AttributeValue> result;
+
+		result = attributeValueRepository.findAll();
+		Assert.notNull(result);
+
+		return result;
+	}
+
+	public AttributeValue findOne(int attributeValueId) {
+		AttributeValue result;
+
+		result = attributeValueRepository.findOne(attributeValueId);
+		Assert.notNull(result);
+
+		return result;
+	}
+
+	public AttributeValue save(AttributeValue attributeValue) {
+		Assert.hasText(attributeValue.getValue(), "El atributeValue debe tener un valor");
+		UserAccount account = loginService.getPrincipal();
+		Assert.isTrue(account.getAuthorities().contains(Authority.ADMINISTRATOR), "Debes ser un Administrador para editar los AttributeValues");
+
+		AttributeValue result;
+		result = attributeValueRepository.save(attributeValue);
+
+		return result;
+	}
+
+	public void delete(AttributeValue attributeValue) {
+		Assert.notNull(attributeValue, "El attributeValueo no puede ser nulo");
+		Assert.isTrue(attributeValue.getId() != 0, "El attributeValueo debe estar en la base de datos");
+		UserAccount account = loginService.getPrincipal();
+		Assert.isTrue(account.getAuthorities().contains(Authority.ADMINISTRATOR), "Debes ser un Administrador para editar los AttributeValues");
+
+		attributeValueRepository.delete(attributeValue);
+	}
+
 }
