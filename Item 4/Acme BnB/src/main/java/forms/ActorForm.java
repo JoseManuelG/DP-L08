d1393aed50a582847005e57157b6a3514299f852
Arturo <artronluc@alum.us.es>
2017-02-21 09:37:58 +0100

    actorForm actualizado

--- Item 4/Acme BnB/src/main/java/forms/ActorForm.java
+++ Item 4/Acme BnB/src/main/java/forms/ActorForm.java
@@ -1,25 +1,30 @@
+
 package forms;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
-import domain.Lessor;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
 
 public class ActorForm {
-	
-	private String  typeOfActor;
-	private Lessor lessor;
-	private boolean conditionsAccepted;
-	
 
-// Constructor -------------------------------------------------------
-	public ActorForm(){
+	private String	typeOfActor;
+	private String	password;
+	private String	name;
+	private String	surname;
+	private String	email;
+	private String	phone;
+	private String	picture;
+	private String	acepted;
+
+
+	//Constructor
+	public ActorForm() {
 		super();
 	}
-//Attributes -------------------------------------------------------
-	
-	@Pattern(regexp ="^LESSOR$|^USER$|^TENANT$|^AUDITOR$")
+	//attributes------------
+	@Pattern(regexp = "^LESSOR$|^TENANT$")
 	public String getTypeOfActor() {
 		return typeOfActor;
 	}
@@ -27,29 +32,59 @@ public class ActorForm {
 	public void setTypeOfActor(String typeOfActor) {
 		this.typeOfActor = typeOfActor;
 	}
-	@NotNull
-	@Valid
-	public Lessor getLessor() {
-		return lessor;
-	}
-	
-	public void setLessor(Lessor lessor) {
-		this.lessor = lessor;
+
+	@NotBlank
+	public String getPassword() {
+		return password;
 	}
 
-	public boolean isConditionsAccepted() {
-		return conditionsAccepted;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
-
-	public void setConditionsAccepted(boolean conditionsAccepted) {
-		this.conditionsAccepted = conditionsAccepted;
+	@NotBlank
+	public String getName() {
+		return name;
 	}
 
-	
+	public void setName(String name) {
+		this.name = name;
+	}
+	@NotBlank
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	@NotBlank
+	@Email
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Pattern(regexp = "^\\+([0-9][0-9][0-9])([0-9A-Za-z])+$")
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	@URL
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	@NotBlank
+	public String getAcepted() {
+		return acepted;
+	}
+	public void setAcepted(String acepted) {
+		this.acepted = acepted;
+	}
 
-
-
-
-	
 }
