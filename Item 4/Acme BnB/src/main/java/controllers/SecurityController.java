ef6c17407399a734240bd7db27daa3e753ae7316
Roldans <roldis2@hotmail.com>
2017-02-23 17:10:31 +0100

    avanzando en la edición de perfil generica

--- Item 4/Acme BnB/src/main/java/controllers/SecurityController.java
+++ Item 4/Acme BnB/src/main/java/controllers/SecurityController.java
@@ -1,6 +1,8 @@
 
 package controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -8,10 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import security.Authority;
+import security.UserAccount;
 import services.ActorService;
+import services.AdministratorService;
+import services.AuditorService;
 import services.LessorService;
 import services.TenantService;
 import domain.Actor;
+import domain.Auditor;
 import domain.Lessor;
 import domain.Tenant;
 import forms.ActorForm;
@@ -27,7 +34,10 @@ public class SecurityController extends AbstractController {
 	LessorService	lessorService;
 	@Autowired
 	ActorService	actorService;
-
+	@Autowired
+	AdministratorService	administratorService;
+	@Autowired
+	AuditorService	auditorService;
 
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public ModelAndView create() {
@@ -77,7 +87,7 @@ public class SecurityController extends AbstractController {
 	
 		@RequestMapping(value="/edit", method=RequestMethod.GET)
 		public ModelAndView edit(){
-			ModelAndView result;
+			ModelAndView result= new ModelAndView("security/edit");
 			Actor actor= actorService .findByPrincipal();
 			ActorForm actorForm= new ActorForm();
 			actorForm.setName(actor.getName());
@@ -88,12 +98,50 @@ public class SecurityController extends AbstractController {
 			
 			actorForm.setUserName(actor.getUserAccount().getUsername());
 			actorForm.setPassword(actor.getUserAccount().getPassword());
+			result.addObject(actorForm);
 			
 			
-			result = createEditModelAndView(actorForm);
 			return result;
 		}
-		
+		@RequestMapping(value="/edit", method = RequestMethod.POST, params = "save")
+		public ModelAndView edit(ActorForm actorForm,BindingResult binding){
+			ModelAndView result;
+			Actor actor= actorService .findByPrincipal();
+			
+			ArrayList<Authority> authorities = new ArrayList<Authority>();
+			authorities.addAll(actor.getUserAccount().getAuthorities());
+			
+			if(authorities.get(0).equals(Authority.AUDITOR)){
+				Auditor auditor= auditorService.findActorByPrincial();
+				auditor.setName(actorForm.getName());
+				auditor.setSurname(actorForm.getSurname());
+				auditor.setEmail(actorForm.getEmail());
+				auditor.setPhone(actorForm.getPhone());
+				auditor.setPicture(actorForm.getPicture());
+				UserAccount account= auditor.getUserAccount();
+				account.setUsername(actorForm.getUserName());
+				account.setPassword(actorForm.getPassword());
+				auditor.setUserAccount(account);
+			}else if(authorities.get(0).toString().equals(Authority.LESSOR)){
+				Lessor lessor= lessorService.findByPrincipal();
+				lessor.setName(actorForm.getName());
+				lessor.setSurname(actorForm.getSurname());
+				lessor.setEmail(actorForm.getEmail());
+				lessor.setPhone(actorForm.getPhone());
+				lessor.setPicture(actorForm.getPicture());
+				UserAccount account= lessor.getUserAccount();
+				account.setUsername(actorForm.getUserName());
+				account.setPassword(actorForm.getPassword());
+				lessor.setUserAccount(account);
+				lessorService.save(lessor);
+			}else if(authorities.get(0).equals(Authority.TENANT)){
+				
+			}
+			result = new ModelAndView("redirect:/");
+			
+			
+			return result;
+		}
 
 	// Ancillary methods ------------------------------------------------------
 
