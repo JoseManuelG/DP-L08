01afb6b165cdb19a15f8ec0f0014cae616733863
ArturoRonda <artronluc@alum.us.es>
2017-06-21 12:00:54 +0200

    Arreglada creditCard

--- Item 4/Acme BnB/src/main/java/controllers/tenant/BookTenantController.java
+++ Item 4/Acme BnB/src/main/java/controllers/tenant/BookTenantController.java
@@ -1,3 +1,4 @@
+
 package controllers.tenant;
 
 import java.util.Collection;
@@ -25,72 +26,69 @@ import forms.BookForm;
 public class BookTenantController extends AbstractController {
 
 	@Autowired
-	private TenantService tenantService;
+	private TenantService		tenantService;
 
 	@Autowired
-	private BookService bookService;
+	private BookService			bookService;
 
 	@Autowired
-	private InvoiceService invoiceService;
+	private InvoiceService		invoiceService;
 
 	@Autowired
-	private CreditCardService creditCardService;
-	
+	private CreditCardService	creditCardService;
+
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public ModelAndView list() {
 		ModelAndView result;
-		
+
 		result = createListModelAndView();
-		
+
 		return result;
 	}
-
 
 	@RequestMapping(value = "/book", method = RequestMethod.GET)
 	public ModelAndView book(@RequestParam int propertyId) {
 		ModelAndView result;
 		BookForm bookForm;
-		
+
 		bookForm = new BookForm();
 		bookForm.setPropertyId(propertyId);
 		result = createEditModelAndView(bookForm);
-		
+
 		return result;
 	}
-	
 
 	@RequestMapping(value = "/book", method = RequestMethod.POST, params = "book")
 	public ModelAndView edit(BookForm bookForm, BindingResult bindingResult) {
 		ModelAndView result;
 		Book book;
-		
+
 		book = bookService.reconstruct(bookForm, bindingResult);
-		if (bindingResult.hasErrors()){
+		if (bindingResult.hasErrors()) {
 			result = createEditModelAndView(bookForm);
 		} else {
-			try{
+			try {
 				bookService.save(book);
 				result = new ModelAndView("redirect:list.do");
 			} catch (IllegalArgumentException e) {
 				result = createEditModelAndView(bookForm, e.getMessage());
 			}
 		}
-		
+
 		return result;
 	}
-
 
 	@RequestMapping(value = "/requestInvoice", method = RequestMethod.GET)
 	public ModelAndView list(@RequestParam int bookId) {
 		ModelAndView result;
 		Book book;
 		Invoice invoice;
-		
+
 		book = bookService.findOne(bookId);
 		invoice = null;
 		try {
-			if (book.getInvoice() == null){
+			if (book.getInvoice() == null) {
 				try {
 					invoice = invoiceService.create(book);
 				} catch (TransactionSystemException e) {
@@ -99,8 +97,8 @@ public class BookTenantController extends AbstractController {
 				bookService.checkOwnerTenantIsPrincipal(book);
 				invoice = book.getInvoice();
 			}
-			result = new ModelAndView("redirect:/invoice/tenant/view.do?invoiceId="+invoice.getId());
-		} catch(IllegalArgumentException e) {
+			result = new ModelAndView("redirect:/invoice/tenant/view.do?invoiceId=" + invoice.getId());
+		} catch (IllegalArgumentException e) {
 			result = createListModelAndView(e.getMessage());
 		}
 		return result;
@@ -110,21 +108,21 @@ public class BookTenantController extends AbstractController {
 		ModelAndView result;
 
 		result = createEditModelAndView(bookForm, null);
-		
+
 		return result;
-	}	
-	
+	}
+
 	protected ModelAndView createEditModelAndView(BookForm bookForm, String message) {
 		ModelAndView result;
 
 		result = new ModelAndView("book/edit");
-		if (bookForm.getCreditCard()!=null && 
-			bookForm.getCreditCard().getNumber().length()==16){
-			try {
-				creditCardService.maskCreditCard(bookForm.getCreditCard());
-			} catch (TransactionSystemException e) {
-			}
-		}
+		//		if (bookForm.getCreditCard()!=null && 
+		//			bookForm.getCreditCard().getNumber().length()==16){
+		//			try {
+		//				creditCardService.maskCreditCard(bookForm.getCreditCard());
+		//			} catch (TransactionSystemException e) {
+		//			}
+		//		}
 		result.addObject("bookForm", bookForm);
 		result.addObject("message", message);
 
@@ -135,27 +133,25 @@ public class BookTenantController extends AbstractController {
 		ModelAndView result;
 
 		result = createListModelAndView(null);
-		
+
 		return result;
 	}
-	
 
 	protected ModelAndView createListModelAndView(String message) {
 		ModelAndView result;
 		Collection<Book> books;
-		
+
 		books = tenantService.findAllBooksByPrincipal();
 		try {
 			creditCardService.maskCreditCardsFromBooks(books);
 		} catch (TransactionSystemException e) {
 		}
 		result = new ModelAndView("book/list");
-		result.addObject("books",books);
-		result.addObject("requestURI","book/tenant/list.do");
+		result.addObject("books", books);
+		result.addObject("requestURI", "book/tenant/list.do");
 		result.addObject("message", message);
-		
+
 		return result;
 	}
-	
-}
 
+}
