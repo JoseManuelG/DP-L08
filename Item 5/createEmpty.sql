867e5063431d782fb628c445eeefb2d5cce63f5c
ArturoRonda <artronluc@alum.us.es>
2017-06-21 17:14:29 +0200

    Arreglado dump

--- Item 5/createEmpty.sql
+++ Item 5/createEmpty.sql
@@ -1,5 +1,21 @@
-CREATE DATABASE  IF NOT EXISTS `acme-bnb` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `acme-bnb`;
+start transaction;
+
+create database `Acme-BnB`;
+
+use `Acme-BnB`;
+
+create user 'acme-user'@'%' identified by password '*4F10007AADA9EE3DBB2CC36575DFC6F4FDE27577';
+
+create user 'acme-manager'@'%' identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
+
+grant select, insert, update, delete 
+on `Acme-BnB`.* to 'acme-user'@'%';
+
+grant select, insert, update, delete, create, drop, references, index, alter, 
+create temporary tables, lock tables, create view, create routine,
+ alter routine, execute, trigger, show view 
+on `Acme-BnB`.* to 'acme-manager'@'%';
+
 -- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
 --
 -- Host: localhost    Database: acme-bnb
@@ -659,3 +675,5 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2017-06-21 17:03:06
+
+commit;
